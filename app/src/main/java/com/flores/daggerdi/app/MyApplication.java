351adac20728363b3c89003830c82f9a32b3a7d9
bill.flores <bill.flores@orbis.com.pe>
2019-01-28 14:23:20 -0500

    update

--- app/src/main/java/com/flores/daggerdi/app/MyApplication.java
+++ app/src/main/java/com/flores/daggerdi/app/MyApplication.java
@@ -1,19 +1,19 @@
-//package com.flores.daggerdi.app;
-//
-//import com.flores.daggerdi.di.Component.AppComponent;
-//
-//import dagger.android.AndroidInjector;
-//import dagger.android.DaggerApplication;
-//
-//public class MyApplication extends DaggerApplication {
-//    @Override
-//    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-//        return null;
-//    }
-//
-//    @Override
-//    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-//
-//        return DaggerAppComponent.builder().application(this).build();
-//    }
-//}
+package com.flores.daggerdi.app;
+
+import com.flores.daggerdi.di.Component.AppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+
+public class MyApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return null;
+    }
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
--- app/src/main/java/com/flores/daggerdi/di/component/AppComponent.java
+++ app/src/main/java/com/flores/daggerdi/di/component/AppComponent.java
@@ -1,13 +1,19 @@
 package com.flores.daggerdi.di.component;
 
+import android.app.Application;
+
 import com.flores.daggerdi.ClassroomActivity;
 import com.flores.daggerdi.di.module.AppModule;
 import com.flores.daggerdi.di.module.ClassroomModule;
 import com.flores.daggerdi.di.module.SchoolModule;
 import com.flores.daggerdi.di.module.StudentsModule;
 import com.flores.daggerdi.di.module.TeacherModule;
+
+import dagger.BindsInstance;
 import dagger.Component;
+import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
+import dagger.android.support.DaggerApplication;
 
 @Component(modules = {
         AppModule.class,
@@ -15,7 +21,15 @@ import dagger.android.support.AndroidSupportInjectionModule;
         StudentsModule.class,
         TeacherModule.class,
         AndroidSupportInjectionModule.class})
-public interface AppComponent {
+public interface AppComponent extends AndroidInjector<DaggerApplication> {
+
+    @Component.Builder
+    interface Builder{
+        @BindsInstance
+        Builder application(Application application);
+        AppComponent build();
+    }
+
 
     SchoolComponent schoolComponent(SchoolModule schoolModule);
 
